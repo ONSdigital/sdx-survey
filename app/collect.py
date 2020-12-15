432b827aea0e42cc2188bd0de19a22d61e0cf57b
Jon Kent <jonkent95@gmail.com>
2020-12-15 16:47:04 +0000

    fix quarantine

--- app/collect.py
+++ app/collect.py
@@ -1,5 +1,3 @@
-import json
-
 from app.publish_dap import send_dap_message
 from app.publish_receipt import send_receipt
 from app.encryption import decrypt_survey, encrypt_survey
@@ -17,29 +15,30 @@ def process(message):
 
     tx_id = extract_tx_id(survey_dict)
 
-    print("validating...")
     try:
+
+        print("validating...")
         validate(survey_dict)
         print("validation successful")
+
+        print("transforming...")
+
+        print("encrypting...")
+        encrypted_survey = encrypt_survey(survey_dict)
+
+        print("write to bucket")
+
+        upload_file(encrypted_survey, tx_id)
+
+        print("receipting...")
+        send_receipt(survey_dict)
+
+        print("send dap notification")
+        send_dap_message(survey_dict)
+
     except ClientError as e:
         print(e)
         quarantine_submission(message_data_str, tx_id)
-
-    print("transforming...")
-
-    print("encrypting...")
-    encrypted_survey = encrypt_survey(survey_dict)
-
-    print("write to bucket")
-
-
-    upload_file(encrypted_survey, tx_id)
-
-    print("receipting...")
-    send_receipt(survey_dict)
-
-    print("send dap notification")
-    send_dap_message(survey_dict)
 
 
 def extract_tx_id(message_dict: dict) -> str:
