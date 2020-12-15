89698bd73bce6fc7d7faccb3cd2b840fe9a43671
Jon Kent <jonkent95@gmail.com>
2020-12-15 16:07:07 +0000

    Added validate functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,15 @@
+import os
+import logging
 from google.cloud import pubsub_v1
+
+LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
+LOGGING_FORMAT = "%(asctime)s.%(msecs)06dZ|%(levelname)s: sdx-worker: %(message)s"
+
+logging.basicConfig(
+    format=LOGGING_FORMAT,
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=LOGGING_LEVEL,
+)
 
 
 project_id = "ons-sdx-sandbox"
@@ -7,6 +18,7 @@ project_id = "ons-sdx-sandbox"
 
 dap_topic_id = "dap-topic"
 receipt_topic_id = "receipt-topic"
+quarantine_topic_id = "quarantine-topic"
 
 dap_publisher = pubsub_v1.PublisherClient()
 # The `topic_path` method creates a fully qualified identifier
@@ -15,6 +27,9 @@ dap_topic_path = dap_publisher.topic_path(project_id, dap_topic_id)
 
 receipt_publisher = pubsub_v1.PublisherClient()
 receipt_topic_path = receipt_publisher.topic_path(project_id, receipt_topic_id)
+
+quarantine_publisher = pubsub_v1.PublisherClient()
+quarantine_topic_path = quarantine_publisher.topic_path(project_id, quarantine_topic_id)
 
 
 # Subscriber config
--- app/collect.py
+++ app/collect.py
@@ -3,7 +3,9 @@ import json
 from app.publish_dap import send_dap_message
 from app.publish_receipt import send_receipt
 from app.encryption import decrypt_survey, encrypt_survey
+from app.quarantine import quarantine_submission
 from app.store import upload_file
+from app.validate import validate, ClientError
 
 
 def process(message):
@@ -13,7 +15,15 @@ def process(message):
     message_data_str = message.data.decode('utf-8')
     survey_dict = decrypt_survey(message_data_str)
 
+    tx_id = extract_tx_id(survey_dict)
+
     print("validating...")
+    try:
+        validate(survey_dict)
+        print("validation successful")
+    except ClientError as e:
+        print(e)
+        quarantine_submission(message_data_str, tx_id)
 
     print("transforming...")
 
@@ -22,7 +32,7 @@ def process(message):
 
     print("write to bucket")
 
-    tx_id = extract_tx_id(survey_dict)
+
     upload_file(encrypted_survey, tx_id)
 
     print("receipting...")
