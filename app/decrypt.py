ab52586bc829028f74263d165fac990bbf6bffa2
TomHolroyd <tomholroyd96@gmail.com>
2021-02-02 12:33:29 +0000

    Adding access to GCP secrets manager

--- app/decrypt.py
+++ app/decrypt.py
@@ -1,12 +1,12 @@
 import binascii
 import logging
-
 import yaml
 from cryptography import exceptions
 from sdc.crypto.exceptions import InvalidTokenException
 from sdc.crypto.key_store import KeyStore
 from sdc.crypto.decrypter import decrypt as sdc_decrypt
 from structlog import wrap_logger
+from app import DECRYPT_KEY
 
 from app.errors import ClientError
 
@@ -19,9 +19,7 @@ def decrypt_survey(payload: str) -> dict:
     logger.info("decrypting survey")
 
     try:
-        with open("./keys2.yaml") as file2:
-            secrets_from_file2 = yaml.safe_load(file2)
-        key_store2 = KeyStore(secrets_from_file2)
+        key_store2 = KeyStore(DECRYPT_KEY)
         decrypted_json = sdc_decrypt(payload, key_store2, KEY_PURPOSE_SUBMISSION)
         logger.info("survey successfully decrypted")
         return decrypted_json
---
+++ app/secrets.py
@@ -0,0 +1,22 @@
+# Import the Secret Manager client library.
+from google.cloud import secretmanager
+
+
+def get_secret(project_id, secret_id):
+    version_id = 'latest'
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": name})
+    # Print the secret payload.
+    #
+    # WARNING: Do not print the secret in a production environment - this
+    # snippet is showing how to access the secret material.
+    payload = response.payload.data.decode("UTF-8")
+    print("Plaintext: {}".format(payload))
+    return payload
