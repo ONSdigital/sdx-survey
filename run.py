49a8c5ad0e2276f8038eda341cc0759d9e981df1
Jon Kent <jonkent95@gmail.com>
2021-06-24 11:08:20 +0100

    Custom output filter

--- run.py
+++ run.py
@@ -1,7 +1,32 @@
+import sys
+
 from app import cloud_config
+
+
+class OutputFilter:
+    """
+        Custom filter to stop "Observed recoverable stream error 503"
+        message from filling up the logs in GCP.
+    """
+
+    def __init__(self, stream):
+        self.stream = stream
+
+    def __getattr__(self, attr_name):
+        return getattr(self.stream, attr_name)
+
+    def write(self, data):
+        if '[code=8a75]' not in data:
+            self.stream.write(data)
+            self.stream.flush()
+
+    def flush(self):
+        self.stream.flush()
+
 
 if __name__ == '__main__':
     print('Starting sdx-survey')
+    sys.stdout = OutputFilter(sys.stdout)
     cloud_config()
     from app import subscriber
     subscriber.start()
