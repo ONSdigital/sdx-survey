27ec2ac40166743607dcbac7c03bf52f9b91b264
Jon Kent <jonkent95@gmail.com>
2021-10-20 17:31:50 +0100

    Read in multiple versions of keys

--- app/decrypt.py
+++ app/decrypt.py
@@ -9,52 +9,62 @@ from sdc.crypto.decrypter import decrypt as sdc_decrypt
 from app import CONFIG
 from app.errors import QuarantinableError
 
-KEY_PURPOSE_SUBMISSION = 'submission'
 
 logger = structlog.get_logger()
 
 
+class Decrypter:
+
+    KEY_PURPOSE_SUBMISSION = 'submission'
+
+    def __init__(self) -> None:
+        keys = [CONFIG.DECRYPT_SURVEY_KEY, CONFIG.AUTHENTICATE_SURVEY_KEY]
+        key_dict = {}
+        for k_list in keys:
+            for k in k_list:
+                key = yaml.safe_load(k)
+                key_dict[key['keyid']] = key
+        self.key_store = KeyStore({"keys": key_dict})
+
+    def decrypt_survey(self, payload: str) -> dict:
+        """
+        Decrypts an encrypted json survey submission
+
+        The payload needs to be a JWE encrypted using SDX's public key.
+        The JWE ciphertext should represent a JWS signed by EQ using
+        their private key and with the survey json as the claims set.
+
+        :param payload:  JWE as a string
+        :return:  The survey submission json as a dictionary
+        """
+
+        logger.info("Decrypting survey")
+
+        try:
+            decrypted_dict = sdc_decrypt(payload, self.key_store, self.KEY_PURPOSE_SUBMISSION)
+            logger.info("Successfully decrypted")
+            return decrypted_dict
+
+        except (
+                exceptions.UnsupportedAlgorithm,
+                exceptions.InvalidKey,
+                exceptions.AlreadyFinalized,
+                exceptions.InvalidSignature,
+                exceptions.NotYetFinalized,
+                exceptions.AlreadyUpdated) as e:
+
+            logger.exception(f"Decryption Failure: {e}")
+            raise QuarantinableError(e)
+        except binascii.Error as e:
+            logger.exception(f"Request payload was not base64 encoded: {e}")
+            raise QuarantinableError(e)
+        except InvalidTokenException as e:
+            logger.exception(repr(e))
+            raise QuarantinableError(e)
+
+
+decrypter = Decrypter()
+
+
 def decrypt_survey(payload: str) -> dict:
-    """
-    Decrypts an encrypted json survey submission
-
-    The payload needs to be a JWE encrypted using SDX's public key.
-    The JWE ciphertext should represent a JWS signed by EQ using
-    their private key and with the survey json as the claims set.
-
-    :param payload:  JWE as a string
-    :return:  The survey submission json as a dictionary
-    """
-
-    logger.info("Decrypting survey")
-
-    try:
-        key_store = load_keys(CONFIG.DECRYPT_SURVEY_KEY, CONFIG.AUTHENTICATE_SURVEY_KEY)
-        decrypted_dict = sdc_decrypt(payload, key_store, KEY_PURPOSE_SUBMISSION)
-        logger.info("Successfully decrypted")
-        return decrypted_dict
-
-    except (
-            exceptions.UnsupportedAlgorithm,
-            exceptions.InvalidKey,
-            exceptions.AlreadyFinalized,
-            exceptions.InvalidSignature,
-            exceptions.NotYetFinalized,
-            exceptions.AlreadyUpdated) as e:
-
-        logger.exception(f"Decryption Failure: {e}")
-        raise QuarantinableError(e)
-    except binascii.Error as e:
-        logger.exception(f"Request payload was not base64 encoded: {e}")
-        raise QuarantinableError(e)
-    except InvalidTokenException as e:
-        logger.exception(repr(e))
-        raise QuarantinableError(e)
-
-
-def load_keys(*keys) -> KeyStore:
-    key_dict = {}
-    for k in keys:
-        key = yaml.safe_load(k)
-        key_dict[key['keyid']] = key
-    return KeyStore({"keys": key_dict})
+    return decrypter.decrypt_survey(payload)
