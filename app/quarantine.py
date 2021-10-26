eca836a86c8881d84449350fbc9f56d2300bcb9b
Jon Kent <jonkent95@gmail.com>
2021-10-26 16:19:03 +0100

    Fixed tests

--- app/quarantine.py
+++ app/quarantine.py
@@ -5,17 +5,9 @@ logger = structlog.get_logger()
 
 
 def quarantine_submission(tx_id: str, error: str):
-    """Publish the submission represented by data_str to the quarantine topic"""
+    """Publish the tx_id and any details about the error to the quarantine topic"""
 
     logger.error("Quarantining submission")
     data = error.encode("utf-8")
     future = CONFIG.QUARANTINE_PUBLISHER.publish(CONFIG.QUARANTINE_TOPIC_PATH, data, tx_id=tx_id, error=error)
-    return future.result()
-
-
-def quarantine_message(message: bytes, tx_id: str, error: str):
-    """Publish the message to the quarantine topic"""
-
-    logger.error("Quarantining message")
-    future = CONFIG.QUARANTINE_PUBLISHER.publish(CONFIG.QUARANTINE_TOPIC_PATH, message, tx_id=tx_id, error=error)
     return future.result()
