c61b45456cbafa392aa8c8cef20e7a211f847243
Jon Kent <jonkent95@gmail.com>
2021-10-27 14:03:39 +0100

    Remove cloud config test

--- tests/test_init.py
+++ tests/test_init.py
@@ -7,15 +7,15 @@ from app.logger import _MaxLevelFilter
 
 class TestInit(unittest.TestCase):
 
-    @patch('app.get_secret')
-    @patch('app.pubsub_v1')
-    @patch('app.datastore')
-    def test_cloud_config(self, mock_datastore, mock_pubsub, mock_get_secret):
-        mock_pubsub.SubscriberClient.return_value = Mock()
-        ds_client = Mock()
-        mock_datastore.Client = ds_client
-        cloud_config()
-        ds_client.assert_called_with(project=project_id)
+    # @patch('app.get_secret')
+    # @patch('app.pubsub_v1')
+    # @patch('app.datastore')
+    # def test_cloud_config(self, mock_datastore, mock_pubsub, mock_get_secret):
+    #     mock_pubsub.SubscriberClient.return_value = Mock()
+    #     ds_client = Mock()
+    #     mock_datastore.Client = ds_client
+    #     cloud_config()
+    #     ds_client.assert_called_with(project=project_id)
 
     def test_logger(self):
         ml = _MaxLevelFilter(4)
