3c29258ff3e9d9fdb664821d67f1d653f738939f
Jon Kent <jonkent95@gmail.com>
2022-09-05 13:41:38 +0100

    Updated

--- app/validate.py
+++ app/validate.py
@@ -1,19 +1,10 @@
-import json
 from pathlib import Path
-
-import structlog
 import jsonschema
+import structlog
 
-from functools import partial
-from structlog.contextvars import bind_contextvars, unbind_contextvars
-from voluptuous import Schema, Required, Length, All, MultipleInvalid, Optional
-from dateutil import parser
-from uuid import UUID
 from app.errors import QuarantinableError
 
 logger = structlog.get_logger('app.subscriber')
-
-schemas = {}
 
 path = Path("./schemas")
 resolver = jsonschema.validators.RefResolver(
@@ -21,84 +12,53 @@ resolver = jsonschema.validators.RefResolver(
     referrer=True,
 )
 
-# A dict of dicts containing lists of formtypes mapped to survey ids
+# A dict of dicts containing lists of form_types mapped to survey ids
 KNOWN_SURVEYS = {
-    "0.0.1": {
-        "007": [
-            "0010", "0009"
-        ],
-        "009": [
-            "0106", "0111", "0117", "0123", "0158", "0161", "0167", "0173", "0201", "0202",
-            "0203", "0204", "0205", "0216", "0251", "0253", "0255", "0817", "0823", "0867",
-            "0873",
-        ],
-        "017": [
-            "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010",
-            "0011", "0012", "0013", "0014", "0033", "0034", "0051", "0052", "0057", "0058",
-            "0061", "0070"
-        ],
-        "019": ["0018", "0019", "0020"],
-        "023": ["0203", "0213", "0205", "0215", "0102", "0112"],
-        "024": ["0002"],
-        "092": ["0001"],
-        "134": ["0005"],
-        "139": ["0001"],
-        "144": ["0001", "0002"],
-        "147": ["0003", "0004"],
-        "160": ["0002"],
-        "165": ["0002"],
-        "169": ["0003"],
-        "182": ["0006"],
-        "183": ["0006"],
-        "184": ["0006"],
-        "185": ["0005"],
-        "187": ["0001", "0002", "0051"],
-        "194": ["0001"],
-        "202": ["1802", "1804", "1808", "1810", "1812", "1814", "1818", "1820", "1824", "1826", "1862", "1864", "1874"],
-        "228": ["0001", "0002"],
-        "283": ["0001", "0002"],
-    },
-    "0.0.2": {
-        "census": ["household", "individual", "communal"],
-        "lms": ["1", "2"],
-        "281": ["0001"],
-    },
+    "007": [
+        "0010", "0009"
+    ],
+    "009": [
+        "0106", "0111", "0117", "0123", "0158", "0161", "0167", "0173", "0201", "0202",
+        "0203", "0204", "0205", "0216", "0251", "0253", "0255", "0817", "0823", "0867",
+        "0873",
+    ],
+    "017": [
+        "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010",
+        "0011", "0012", "0013", "0014", "0033", "0034", "0051", "0052", "0057", "0058",
+        "0061", "0070"
+    ],
+    "019": ["0018", "0019", "0020"],
+    "023": ["0203", "0213", "0205", "0215", "0102", "0112"],
+    "024": ["0002"],
+    "092": ["0001"],
+    "134": ["0005"],
+    "139": ["0001"],
+    "144": ["0001", "0002"],
+    "147": ["0003", "0004"],
+    "160": ["0002"],
+    "165": ["0002"],
+    "169": ["0003"],
+    "182": ["0006"],
+    "183": ["0006"],
+    "184": ["0006"],
+    "185": ["0005"],
+    "187": ["0001", "0002", "0051"],
+    "194": ["0001"],
+    "202": ["1802", "1804", "1808", "1810", "1812", "1814", "1818", "1820", "1824", "1826", "1862", "1864", "1874"],
+    "228": ["0001", "0002"],
+    "283": ["0001", "0002"]
 }
 
 
-def parse_timestamp(value):
-    """Parses a timestamp, throwing a value error if unrecognised"""
-    return parser.parse(value)
-
-
-def is_valid_survey_id(value, version=None):
+def check_known_survey(survey_id: str, form_type: str):
     """Tests if a survey id is valid"""
-    if not version:
-        raise AttributeError("No version number")
+    if survey_id not in KNOWN_SURVEYS:
+        logger.error("Survey id is not known", survey_id=survey_id)
+        raise ValueError(f"Invalid survey id: {survey_id}")
 
-    if value not in KNOWN_SURVEYS[version]:
-        raise ValueError("Invalid survey id")
-
-
-def is_valid_survey_txid(value):
-    """Parses a UUID, throwing a value error if unrecognised"""
-    return UUID(value, version=4)
-
-
-def is_valid_survey_data(data):
-    """Tests if a survey data is of valid types"""
-    if isinstance(data, dict):
-        for k, v in data.items():
-            if not isinstance(k, str) or not isinstance(v, (str, list, int, float)):
-                raise ValueError("Invalid survey data")
-
-    else:
-        raise ValueError("Invalid survey data")
-
-
-def is_valid_list_survey_data(data):
-    if not isinstance(data, list):
-        raise ValueError("Invalid survey data")
+    if form_type not in KNOWN_SURVEYS[survey_id]:
+        logger.error("form_type is not known", survey_id=survey_id, form_type=form_type)
+        raise ValueError(f"Invalid form_type: {form_type}")
 
 
 def validate(survey_dict: dict) -> bool:
@@ -107,56 +67,26 @@ def validate(survey_dict: dict) -> bool:
     Returns True if valid or raises an appropriate exception if not.
     """
 
-    logger.info(f"Validating")
     try:
         json_data = survey_dict
-        if json_data.get('type') is None:
-            raise QuarantinableError('Missing type field')
 
-        response_type = str(json_data["type"])
+        logger.info("Validating json against schema")
 
-        if response_type.find("feedback") == -1:
-            version = json_data["version"]
+        jsonschema.validate(
+            instance=json_data,
+            schema={"$ref": "submission_v1.json"},
+            resolver=resolver,
+        )
 
-            metadata = json_data.get("metadata")
-            if metadata is None:
-                raise QuarantinableError('Missing metadata field')
-            bind_contextvars(survey_id=json_data.get("survey_id"),
-                             user_id=metadata.get("user_id"),
-                             ru_ref=metadata.get("ru_ref"))
+        survey_id = json_data.get("survey_id")
+        form_type = json_data.get("collection", {}).get("instrument_id")
 
-            logger.info("Validating json against schema")
-
-            jsonschema.validate(
-                instance=json_data,
-                schema={"$ref": "submission_v1.json"},
-                resolver=resolver,
-            )
-
-            survey_id = json_data.get("survey_id")
-            if survey_id not in KNOWN_SURVEYS.get(version, {}):
-                logger.error("Survey id is not known", survey_id=survey_id)
-                raise QuarantinableError(f"Unsupported survey '{survey_id}'")
-
-            instrument_id = json_data.get("collection", {}).get("instrument_id")
-            if instrument_id not in KNOWN_SURVEYS.get(version, {}).get(survey_id, []):
-                logger.error("Instrument ID is not known", survey_id=survey_id)
-                raise QuarantinableError(f"Unsupported instrument '{instrument_id}'")
-
-        else:
-            schema = get_schema("feedback")
-
-            bind_contextvars(response_type="feedback")
-            logger.info("Validating json against schema")
-            schema(json_data)
-
-        logger.debug("Success")
+        try:
+            check_known_survey(survey_id, form_type)
+        except ValueError:
+            raise QuarantinableError(f"Unsupported survey_id and/or form_type '{survey_id}:{form_type}'")
 
     except jsonschema.ValidationError as e:
-        # logger.error("Client error", error=e)
-        raise QuarantinableError(e)
-
-    except (MultipleInvalid, KeyError, TypeError, ValueError) as e:
         logger.error("Client error", error=e)
         raise QuarantinableError(e)
 
@@ -165,95 +95,4 @@ def validate(survey_dict: dict) -> bool:
         raise QuarantinableError(e)
 
     logger.info(f"Validation successful")
-    unbind_contextvars('survey_id',
-                       'response_type',
-                       'user_id',
-                       'ru_ref')
     return True
-
-
-def get_schema(version):
-    if version == "0.0.1":
-        return schemas["survey"]
-
-    elif version == "0.0.2":
-        valid_survey_id = partial(is_valid_survey_id, version="0.0.2")
-
-        collection_s = Schema(
-            {
-                Required("period"): str,
-                Required("exercise_sid"): str,
-                Required("instrument_id"): All(str, Length(max=10)),
-            }
-        )
-
-        metadata_s = Schema(
-            {
-                Required("user_id"): str,
-                Optional("ru_ref"): str,
-                Optional("ref_period_start_date"): parse_timestamp,
-                Optional("ref_period_end_date"): parse_timestamp,
-            }
-        )
-
-        schema = Schema(
-            {
-                Required("type"): "uk.gov.ons.edc.eq:surveyresponse",
-                Required("version"): "0.0.2",
-                Optional("tx_id"): All(str, is_valid_survey_txid),
-                Required("origin"): "uk.gov.ons.edc.eq",
-                Required("survey_id"): All(str, valid_survey_id),
-                Optional("completed"): bool,
-                Optional("flushed"): bool,
-                Optional("started_at"): parse_timestamp,
-                Required("submitted_at"): parse_timestamp,
-                Required("case_id"): str,
-                Optional("case_ref"): str,
-                Required("collection"): collection_s,
-                Required("metadata"): metadata_s,
-                Required("data"): is_valid_list_survey_data,
-                Optional("paradata"): object,
-            }
-        )
-        return schema
-
-    elif version == "feedback":
-
-        collection_s = Schema(
-            {
-                Required("period"): str,
-                Required("exercise_sid"): str,
-                Required("instrument_id"): All(str, Length(max=10)),
-                # Added for eq_v3
-                Optional("schema_name"): str,
-            }
-        )
-
-        schema = Schema(
-            {
-                Required("type"): "uk.gov.ons.edc.eq:feedback",
-                Optional("tx_id"): All(str, is_valid_survey_txid),
-                Required("origin"): "uk.gov.ons.edc.eq",
-                Required("survey_id"): str,
-                Required("version"): str,
-                Optional("completed"): bool,
-                Optional("flushed"): bool,
-                Optional("started_at"): parse_timestamp,
-                Required("submitted_at"): parse_timestamp,
-                Required("collection"): collection_s,
-                Required("data"): is_valid_survey_data,
-                Optional("metadata"): object,
-                Optional("paradata"): object,
-                # Added for eq_v3
-                Optional("case_id"): str,
-                Optional("launch_language_code"): str,
-                Optional("submission_language_code"): str,
-                Optional("form_type"): str,
-            }
-        )
-        return schema
-
-    else:
-        return None
-
-
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch, Mock
 
 from app.errors import QuarantinableError
-from app.validate import validate, is_valid_survey_id, is_valid_survey_data
+from app.validate import validate, check_known_survey
 
 import unittest
 import json
@@ -12,13 +12,12 @@ class TestValidateService(unittest.TestCase):
     def setUp(self):
 
         self.message = {
-            '0.0.1': '''{
+            'submission_v1': '''{
                "tx_id": "0f534ffc-9442-414c-b39f-a756b4adc6cb",
                "type": "uk.gov.ons.edc.eq:surveyresponse",
                "origin": "uk.gov.ons.edc.eq",
                "survey_id": "023",
                "case_id": "12345678-1234-1234-1234-123456789012",
-               "completed": true,
                "flushed": false,
                "version": "0.0.1",
                "collection": {
@@ -50,36 +49,6 @@ class TestValidateService(unittest.TestCase):
                  "27": "7400",
                  "146": "some comment"
                }
-            }''',
-
-            '0.0.2': '''{
-               "tx_id": "0f534ffc-9442-414c-b39f-a756b4adc6cb",
-               "case_ref": "1000000000000001",
-               "case_id": "4c0bc9ec-06d4-4f66-88b6-2e42b79f17b3",
-               "type": "uk.gov.ons.edc.eq:surveyresponse",
-               "origin": "uk.gov.ons.edc.eq",
-               "survey_id": "census",
-               "completed": false,
-               "flushed": true,
-               "version": "0.0.2",
-               "collection": {
-                 "exercise_sid": "hfjdskf",
-                 "instrument_id": "household",
-                 "period": "0216"
-               },
-               "submitted_at": "2016-03-12T10:39:40Z",
-               "metadata": {
-                 "user_id": "789473423",
-                 "ru_ref": "12345"
-               },
-               "data": [{
-                 "value": "Joe Bloggs",
-                 "block_id": "household-composition",
-                 "answer_id": "household-full-name",
-                 "group_id": "multiple-questions-group",
-                 "group_instance": 0,
-                 "answer_instance": 0
-                }]
             }''',
 
             'feedback': '''{
@@ -151,13 +120,8 @@ class TestValidateService(unittest.TestCase):
     def assertValid(self, data):
         self.assertTrue(self.validate_response(data))
 
-    # def test_unexpected_payload_data(self):
-    #     survey = json.loads(self.message['0.0.1'])
-    #     survey['unexpected_data'] = 'unexpected'
-    #     self.assertInvalid(survey)
-
     def test_validates_json(self):
-        for v in ['0.0.1', 'feedback']:
+        for v in ['submission_v1', 'feedback']:
             m = self.message[v]
             self.assertValid(m)
 
@@ -166,19 +130,19 @@ class TestValidateService(unittest.TestCase):
         self.assertValid(m)
 
     def test_mwss_valid(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         survey['survey_id'] = "134"
         survey['collection']['instrument_id'] = "0005"
         self.assertValid(survey)
 
     def test_mbs_valid(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         survey['survey_id'] = "009"
         survey['collection']['instrument_id'] = "0255"
         self.assertValid(survey)
 
     def test_epe_valid(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         survey['survey_id'] = "147"
 
         survey['collection']['instrument_id'] = "0003"
@@ -188,7 +152,7 @@ class TestValidateService(unittest.TestCase):
         self.assertValid(survey)
 
     def test_qcas_valid(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         survey['survey_id'] = "019"
 
         survey['collection']['instrument_id'] = "0018"
@@ -206,126 +170,79 @@ class TestValidateService(unittest.TestCase):
     def test_rsi_metadata(self):
         for inst_id in ["0102", "0112", "0203", "0205", "0213", "0215"]:
             with self.subTest(inst_id=inst_id):
-                survey = json.loads(self.message["0.0.1"])
+                survey = json.loads(self.message["submission_v1"])
                 survey["survey_id"] = "023"
                 survey["collection"]["instrument_id"] = inst_id
                 survey["metadata"]["ref_period_start_date"] = "2016-04-01"
                 survey["metadata"]["ref_period_end_date"] = "2016-10-31"
                 self.assertValid(survey)
 
-    def test_invalid_version(self):
-        survey = json.loads(self.message['0.0.1'])
-        with self.assertRaises(KeyError):
-            is_valid_survey_id(survey['survey_id'], '0.0.3')
+    def test_unknown_survey_id(self):
+        with self.assertRaises(ValueError):
+            check_known_survey("300", "")
 
-    def test_missing_version(self):
-        survey = json.loads(self.message['0.0.1'])
-        with self.assertRaises(AttributeError):
-            is_valid_survey_id(survey['survey_id'])
+    def test_unknown_formtype(self):
+        with self.assertRaises(ValueError):
+            check_known_survey("134", "0006")
 
-    def test_valid_version(self):
-        survey = json.loads(self.message['0.0.1'])
-        is_valid_survey_id(survey['survey_id'], survey['version'])
+    def test_known_survey_id_and_formtype(self):
+        check_known_survey("134", "0005")
 
     def test_unknown_version_invalid(self):
-        unknown_version = json.loads(self.message['0.0.1'])
+        unknown_version = json.loads(self.message['submission_v1'])
         unknown_version['version'] = "0.0.3"
 
         self.assertInvalid(unknown_version)
 
     def test_unknown_survey_invalid(self):
-        unknown_survey = json.loads(self.message['0.0.1'])
+        unknown_survey = json.loads(self.message['submission_v1'])
         unknown_survey['survey_id'] = "025"
 
         self.assertInvalid(unknown_survey)
 
     def test_blank_survey_invalid(self):
-        unknown_survey = json.loads(self.message['0.0.1'])
+        unknown_survey = json.loads(self.message['submission_v1'])
         unknown_survey['survey_id'] = ""
 
         self.assertInvalid(unknown_survey)
 
     def test_missing_survey_invalid(self):
-        unknown_survey = json.loads(self.message['0.0.1'])
+        unknown_survey = json.loads(self.message['submission_v1'])
         del unknown_survey['survey_id']
 
         self.assertInvalid(unknown_survey)
 
-    # def test_unknown_census_survey_invalid(self):
-    #     unknown_survey = json.loads(self.message['0.0.2'])
-    #     unknown_survey['survey_id'] = "025"
-
-        self.assertInvalid(unknown_survey)
-
     def test_unknown_instrument_invalid(self):
-        unknown_instrument = json.loads(self.message['0.0.1'])
+        unknown_instrument = json.loads(self.message['submission_v1'])
         unknown_instrument['collection']['instrument_id'] = "999"
-
-        self.assertInvalid(unknown_instrument)
-
-    # def test_unknown_census_instrument_invalid(self):
-    #     unknown_instrument = json.loads(self.message['0.0.2'])
-    #     unknown_instrument['collection']['instrument_id'] = "999"
 
         self.assertInvalid(unknown_instrument)
 
     def test_known_instrument_wrong_survey_invalid(self):
         # RSI survey_id with Census instrument_id
-        known_instrument = json.loads(self.message['0.0.1'])
+        known_instrument = json.loads(self.message['submission_v1'])
         known_instrument['collection']['instrument_id'] = "household"
-
-        self.assertInvalid(known_instrument)
-
-    # def test_known_census_instrument_wrong_survey_invalid(self):
-    #     # RSI survey_id with Census instrument_id
-    #     known_instrument = json.loads(self.message['0.0.2'])
-    #     known_instrument['collection']['instrument_id'] = "0203"
 
         self.assertInvalid(known_instrument)
 
     def test_known_instrument_correct_survey_valid(self):
         # RSI survey_id with RSI instrument_id
-        known_instrument = json.loads(self.message['0.0.1'])
+        known_instrument = json.loads(self.message['submission_v1'])
         known_instrument['collection']['instrument_id'] = "0213"
 
         self.assertValid(known_instrument)
 
-    # def test_known_census_instrument_correct_survey_valid(self):
-    #     # RSI survey_id with RSI instrument_id
-    #     known_instrument = json.loads(self.message['0.0.2'])
-    #     known_instrument['collection']['instrument_id'] = "household"
-    #
-    #     self.assertValid(known_instrument)
-
     def test_empty_data_invalid(self):
-        empty_data = json.loads(self.message['0.0.1'])
+        empty_data = json.loads(self.message['submission_v1'])
         empty_data['data'] = ""
 
         self.assertInvalid(empty_data)
-
-    # def test_census_empty_data_invalid(self):
-    #     empty_data = json.loads(self.message['0.0.2'])
-    #     empty_data['data'] = ""
-    #
-    #     self.assertInvalid(empty_data)
 
     def test_feedback_empty_data_invalid(self):
         empty_data = json.loads(self.message['feedback'])
         empty_data['data'] = ""
 
         self.assertInvalid(empty_data)
-
-    # def test_census_dict_data_invalid(self):
-    #     dict_data = json.loads(self.message['0.0.2'])
-    #     dict_data['data'] = {'key1': 'value1', 'key2': 'value2'}
-    #
-    #     self.assertInvalid(dict_data)
-
-    # def test_census_list_dict_plain_data_valid(self):
-    #     data = json.loads(self.message['0.0.2'])
-    #     data['data'] = ["a", "b", "c", {"Some": "Thing"}]
-    #
-    #     self.assertValid(data)
 
     def test_string_data_invalid(self):
         data = "abcd"
@@ -338,7 +255,7 @@ class TestValidateService(unittest.TestCase):
         self.assertInvalid(data)
 
     def test_non_guid_tx_id_invalid(self):
-        wrong_tx = json.loads(self.message['0.0.1'])
+        wrong_tx = json.loads(self.message['submission_v1'])
         wrong_tx['tx_id'] = "999"
 
         self.assertInvalid(wrong_tx)
@@ -349,97 +266,64 @@ class TestValidateService(unittest.TestCase):
         self.assertInvalid(wrong_tx)
 
     def test_missing_tx_id_is_invalid(self):
-        message = json.loads(self.message['0.0.1'])
+        message = json.loads(self.message['submission_v1'])
         del message['tx_id']
 
         self.assertInvalid(message)
 
-    # def test_completed_not_boolean_fails(self):
-    #     message = json.loads(self.message['0.0.1'])
-    #     message['completed'] = ''
-    #
-    #     self.assertInvalid(message)
-
     def test_flushed_not_boolean_fails(self):
-        message = json.loads(self.message['0.0.1'])
+        message = json.loads(self.message['submission_v1'])
         message['flushed'] = ''
 
         self.assertInvalid(message)
 
-    # def test_completed_key_missing_fails(self):
-    #     message = json.loads(self.message['0.0.1'])
-    #     message.pop('completed')
-    #
-    #     self.assertRaises(KeyError, message.__getitem__, 'completed')
-    #     self.assertValid(message)
-
     def test_flushed_key_missing_fails(self):
-        message = json.loads(self.message['0.0.1'])
+        message = json.loads(self.message['submission_v1'])
         message.pop('flushed')
 
         self.assertInvalid(message)
 
     def test_case_id_and_case_ref_passes(self):
-        message = json.loads(self.message['0.0.1'])
+        message = json.loads(self.message['submission_v1'])
         self.assertValid(message)
 
     def test_case_id_not_str_fails(self):
-        message = json.loads(self.message['0.0.1'])
+        message = json.loads(self.message['submission_v1'])
         message['case_id'] = {}
 
         self.assertInvalid(message)
 
-    # def test_case_ref_not_str_fails(self):
-    #     message = json.loads(self.message['0.0.1'])
-    #     message['case_ref'] = {}
-    #
-    #     self.assertInvalid(message)
-
-    # def test_valid_lms(self):
-    #     survey = json.loads(self.message['0.0.2'])
-    #     survey['survey_id'] = "lms"
-    #     survey['collection']['instrument_id'] = "1"
-    #
-    #     self.assertValid(survey)
-
-    def test_is_valid_survey_data_value_error(self):
-        data = {"tx_id": {}}
-        with self.assertRaises(ValueError):
-            is_valid_survey_data(data)
-
     def test_validate_type_none_is_quarantined(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         del survey["type"]
         with self.assertRaises(QuarantinableError):
             validate(survey)
 
     def test_validate_meta_none_is_quarantined(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         del survey["metadata"]
         with self.assertRaises(QuarantinableError):
             validate(survey)
 
     def test_not_known_survey(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         survey["survey_id"] = "567"
         with self.assertRaises(QuarantinableError):
             validate(survey)
 
-    @patch('app.validate.get_schema')
-    def test_not_known_survey_id(self, mock_get_schema):
-        mock_get_schema.return_value = Mock()
-        survey = json.loads(self.message['0.0.1'])
+    def test_not_known_survey_id(self):
+        survey = json.loads(self.message['submission_v1'])
         survey["survey_id"] = "567"
         with self.assertRaises(QuarantinableError):
             validate(survey)
 
     def test_allow_case_type(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         survey['case_type'] = "HH"
         self.assertValid(survey)
 
     def test_allow_all_eq_v3_fields(self):
-        survey = json.loads(self.message['0.0.1'])
+        survey = json.loads(self.message['submission_v1'])
         survey['collection']['schema_name'] = "mbs_1704"
         survey['metadata']['ref_period_start_date'] = "2016-05-01"
         survey['metadata']['ref_period_end_date'] = "2016-05-31"
@@ -452,5 +336,3 @@ class TestValidateService(unittest.TestCase):
         survey['channel'] = "RAS"
 
         self.assertValid(survey)
-
-
