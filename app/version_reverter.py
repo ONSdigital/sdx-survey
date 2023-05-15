9782002600afcc1ff398796391bcf32a7f3a4c37
ldearsley <ldearsley2@gmail.com>
2023-05-15 15:27:02 +0100

    add version reverter and added notes for the needed functionality

---
+++ app/version_reverter.py
@@ -0,0 +1,27 @@
+# change version: v2 to version: 0.0.1
+
+# remove data version
+
+# create collection object
+
+# add collection_exercise_sid to collection object
+
+# add schema_name to collection object
+
+# remove flushed attribute
+
+# move survey_id into outside object
+
+# move period_id to collection object as period
+
+# move user_id from survey_metadata to meta_data object
+
+# move ru_ref from survey_metadata to meta_data object
+
+# move ref_p_start_date from survey_metadata to metadata as ref_period_start_date
+
+# move ref_p_end_date from survey_metadata to metadata as ref_period_end_date
+
+# move form_type from survey_metadata into outside object
+
+# move form_type from survey_metadata into collection object as instrument_id
