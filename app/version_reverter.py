2d6e0bb8e8897d6f7c2731ec95eecacefc34d658
ldearsley <ldearsley2@gmail.com>
2023-05-23 10:02:39 +0100

    add version reverter and added notes for the needed functionality

--- app/version_reverter.py
+++ app/version_reverter.py
@@ -25,3 +25,40 @@
 # move form_type from survey_metadata into outside object
 
 # move form_type from survey_metadata into collection object as instrument_id
+from typing import Dict
+
+from app.submission_type import get_field
+
+
+class VersionReverter:
+
+    def revert_data_to_v1(self, submission: Dict):
+        v1_template = {
+            "case_id": get_field(submission, "case_id"),
+            "tx_id": get_field(submission, "tx_id"),
+            "type": get_field(submission, "type"),
+            "version": get_field(submission, "data_version"),
+            "origin": get_field(submission, "origin"),
+            "survey_id": get_field(submission, "survey_metadata", "survey_id"),
+            "flushed": get_field(submission, "flushed"),
+            "submitted_at": get_field(submission, "submitted_at"),
+            "collection": {
+                "exercise_sid": get_field(submission, "collection_exercise_sid"),
+                "schema_name": get_field(submission, "schema_name"),
+                "period": get_field(submission, "survey_metadata", "period_id"),
+                "instrument_id": get_field(submission, "survey_metadata", "form_type")
+            },
+            "metadata": {
+                "user_id": get_field(submission, "survey_metadata", "user_id"),
+                "ru_ref": get_field(submission, "survey_metadata", "ru_ref"),
+                "ref_period_start_date": get_field(submission, "survey_metadata", "ref_p_start_date"),
+                "ref_period_end_date": get_field(submission, "survey_metadata", "ref_p_end_date")
+            },
+            "launch_language_code": get_field(submission, "launch_language_code"),
+            "data": get_field(submission, "data"),
+            "form_type": get_field(submission, "survey_metadata", "form_type"),
+            "started_at": get_field(submission, "started_at"),
+            "submission_language_code": get_field(submission, "submission_language_code")
+        }
+
+        return v1_template
