2524fc4999551e760e2803e09d82d45b91b0fb00
ldearsley <ldearsley2@gmail.com>
2023-05-23 14:01:13 +0100

    add version reverter and added notes for the needed functionality

--- app/version_reverter.py
+++ app/version_reverter.py
@@ -1,64 +1,67 @@
-# change version: v2 to version: 0.0.1
+from sdx_gcp.errors import DataError
+from sdx_gcp.app import get_logger
 
-# remove data version
+from app.submission_type import get_field, get_survey_type, SurveyType, get_response_type, ResponseType, \
+    get_schema_version, SchemaVersion, get_deliver_target, DeliverTarget
 
-# create collection object
+logger = get_logger()
 
-# add collection_exercise_sid to collection object
-
-# add schema_name to collection object
-
-# remove flushed attribute
-
-# move survey_id into outside object
-
-# move period_id to collection object as period
-
-# move user_id from survey_metadata to meta_data object
-
-# move ru_ref from survey_metadata to meta_data object
-
-# move ref_p_start_date from survey_metadata to metadata as ref_period_start_date
-
-# move ref_p_end_date from survey_metadata to metadata as ref_period_end_date
-
-# move form_type from survey_metadata into outside object
-
-# move form_type from survey_metadata into collection object as instrument_id
-from typing import Dict
-
-from app.submission_type import get_field
+Submission = dict[str, str | dict[str, str]]
 
 
-class VersionReverter:
+def get_optional(submission: dict, *field_names: str) -> str:
+    try:
+        return get_field(submission, *field_names)
 
-    def revert_data_to_v1(self, submission: Dict):
-        v1_template = {
-            "case_id": get_field(submission, "case_id"),
-            "tx_id": get_field(submission, "tx_id"),
-            "type": get_field(submission, "type"),
-            "version": get_field(submission, "data_version"),
-            "origin": get_field(submission, "origin"),
-            "survey_id": get_field(submission, "survey_metadata", "survey_id"),
-            "flushed": get_field(submission, "flushed"),
-            "submitted_at": get_field(submission, "submitted_at"),
-            "collection": {
-                "exercise_sid": get_field(submission, "collection_exercise_sid"),
-                "schema_name": get_field(submission, "schema_name"),
-                "period": get_field(submission, "survey_metadata", "period_id"),
-                "instrument_id": get_field(submission, "survey_metadata", "form_type")
-            },
-            "metadata": {
-                "user_id": get_field(submission, "survey_metadata", "user_id"),
-                "ru_ref": get_field(submission, "survey_metadata", "ru_ref"),
-                "ref_period_start_date": get_field(submission, "survey_metadata", "ref_p_start_date"),
-                "ref_period_end_date": get_field(submission, "survey_metadata", "ref_p_end_date")
-            },
-            "launch_language_code": get_field(submission, "launch_language_code"),
-            "data": get_field(submission, "data"),
-            "form_type": get_field(submission, "survey_metadata", "form_type"),
-            "started_at": get_field(submission, "started_at"),
-            "submission_language_code": get_field(submission, "submission_language_code")
-        }
+    except DataError as e:
+        logger.warn(str(e))
+        return ""
 
-        return v1_template
+
+def requires_converting(submission: Submission) -> bool:
+
+    if get_survey_type(submission) != SurveyType.BUSINESS:
+        return False
+
+    if get_response_type(submission) != ResponseType.SURVEY:
+        return False
+
+    if get_schema_version(submission) != SchemaVersion.V2:
+        return False
+
+    if get_deliver_target(submission) != DeliverTarget.LEGACY:
+        return False
+
+    return True
+
+
+def convert_v2_to_v1(submission: Submission) -> Submission:
+    v1_template = {
+        "case_id": get_field(submission, "case_id"),
+        "tx_id": get_field(submission, "tx_id"),
+        "type": get_field(submission, "type"),
+        "version": get_field(submission, "data_version"),
+        "origin": get_optional(submission, "origin"),
+        "survey_id": get_field(submission, "survey_metadata", "survey_id"),
+        "flushed": get_optional(submission, "flushed"),
+        "submitted_at": get_field(submission, "submitted_at"),
+        "collection": {
+            "exercise_sid": get_field(submission, "collection_exercise_sid"),
+            "schema_name": get_field(submission, "schema_name"),
+            "period": get_field(submission, "survey_metadata", "period_id"),
+            "instrument_id": get_field(submission, "survey_metadata", "form_type")
+        },
+        "metadata": {
+            "user_id": get_field(submission, "survey_metadata", "user_id"),
+            "ru_ref": get_field(submission, "survey_metadata", "ru_ref"),
+            "ref_period_start_date": get_field(submission, "survey_metadata", "ref_p_start_date"),
+            "ref_period_end_date": get_field(submission, "survey_metadata", "ref_p_end_date")
+        },
+        "launch_language_code": get_optional(submission, "launch_language_code"),
+        "data": get_field(submission, "data"),
+        "form_type": get_field(submission, "survey_metadata", "form_type"),
+        "started_at": get_optional(submission, "started_at"),
+        "submission_language_code": get_optional(submission, "submission_language_code")
+    }
+
+    return v1_template
