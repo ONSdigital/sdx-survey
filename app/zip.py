4fcabf124cbe9ffdb1c92eee3ca3aefe642e4d02
Angus Goody <angus.goody@gmail.com>
2023-11-02 16:23:01 +0000

    formatted

--- app/zip.py
+++ app/zip.py
@@ -3,12 +3,10 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 
 def create_zip(files: dict[str, str]) -> BytesIO:
+    archive = BytesIO()
 
-	archive = BytesIO()
+    with ZipFile(archive, "w", ZIP_DEFLATED, False) as zip_archive:
+        for filename, file_contents in files.items():
+            zip_archive.writestr(filename, file_contents)
 
-	with ZipFile(archive, "w", ZIP_DEFLATED, False) as zip_archive:
-
-		for filename, file_contents in files.items():
-			zip_archive.writestr(filename, file_contents)
-
-	return archive
+    return archive
--- tests/test_zip.py
+++ tests/test_zip.py
@@ -6,27 +6,27 @@ from app.zip import create_zip
 
 
 def extract_zip(zip_bytes: bytes) -> dict:
-	z = zipfile.ZipFile(io.BytesIO(zip_bytes), "r")
-	files = {}
-	for filename in z.namelist():
-		file_bytes = z.read(filename)
-		files[filename] = file_bytes
+    z = zipfile.ZipFile(io.BytesIO(zip_bytes), "r")
+    files = {}
+    for filename in z.namelist():
+        file_bytes = z.read(filename)
+        files[filename] = file_bytes
 
-	z.close()
-	return files
+    z.close()
+    return files
+
 
 class TestZip(unittest.TestCase):
 
-	def test_zip(self):
+    def test_zip(self):
+        my_files = {
+            "file1": "contents for file 1",
+            "file2": "contents for file 2",
+            "file3": "contents for file 3"
+        }
 
-		my_files = {
-			"file1": "contents for file 1",
-			"file2": "contents for file 2",
-			"file3": "contents for file 3"
-		}
+        zip_contents = create_zip(my_files)
 
-		zip_contents = create_zip(my_files)
+        extracted_zip_files = extract_zip(zip_contents)
 
-		extracted_zip_files = extract_zip(zip_contents)
-
-		self.assertDictEqual(extracted_zip_files, my_files)
+        self.assertDictEqual(extracted_zip_files, my_files)
