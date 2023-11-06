0c776d5886210491dedcc733bb2d0ae29183c1c0
Angus Goody <angus.goody@gmail.com>
2023-11-06 12:14:26 +0000

    added transform test

--- app/transform/transform.py
+++ app/transform/transform.py
@@ -5,14 +5,14 @@ from app.transform.zip import create_zip
 
 
 def transform(submission: SurveySubmission) -> bytes:
-    files = {}
+    files: dict[str, bytes] = {}
 
     image_name = image.get_name(submission)
 
-    files[pck.get_name(submission)] = pck.get_contents(submission)
-    files[image_name] = image.get_image(submission)
-    files[index.get_name(submission)] = index.get_contents(submission, image_name)
-    files[idbr.get_name(submission)] = idbr.get_contents(submission)
-    files[json.get_name(submission)] = json.get_contents(submission)
+    files[f"EDC_QData/{pck.get_name(submission)}"] = pck.get_contents(submission)
+    files[f"EDC_QImages/Images/{image_name}"] = image.get_image(submission)
+    files[f"EDC_QImages/Index/{index.get_name(submission)}"] = index.get_contents(submission, image_name)
+    files[f"EDC_QReceipts/{idbr.get_name(submission)}"] = idbr.get_contents(submission)
+    files[f"EDC_QJson/{json.get_name(submission)}"] = json.get_contents(submission)
 
     return create_zip(files)
