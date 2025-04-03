57832951bc60124637e93c4f2c90b2a78e206407
Jon Kent <29626876+Jon-Kent@users.noreply.github.com>
2025-04-03 15:37:01 +0100

    testing v2

--- app/submission_type.py
+++ app/submission_type.py
@@ -1,5 +1,6 @@
 from sdx_gcp.app import get_logger
 
+from app import CONFIG
 from app.response import Response, SurveyType, ResponseType, SchemaVersion, DeliverTarget
 
 """
@@ -31,7 +32,7 @@ _JSON_NAME_CHANGE = ["024", "068", "071", "194"]
 _JSON_TRANSFORM = ["002"]
 
 # responses that will use the v2 schema for messaging Nifi
-_V2_NIFI_MESSAGE = []
+_V2_NIFI_MESSAGE = ["002", "007", "009", "023", "068", "139", "228"]
 
 
 def requires_v1_conversion(response: Response) -> bool:
@@ -86,11 +87,9 @@ def is_v2_nifi_message_submission(response: Response) -> bool:
     """
     Returns True if this response is configured to use the v2 nifi message schema.
     """
-    # if CONFIG.PROJECT_ID == "ons-sdx-prod" or CONFIG.PROJECT_ID == "ons-sdx-ci":
-    #     return False
-    #
-    # if response.get_response_type() == ResponseType.FEEDBACK:
-    #     return True
-    # return response.get_survey_id() in _V2_NIFI_MESSAGE
+    if CONFIG.PROJECT_ID == "ons-sdx-preprod" or CONFIG.PROJECT_ID == "ons-sdx-nifi":
+        if response.get_response_type() == ResponseType.FEEDBACK:
+            return True
+        return response.get_survey_id() in _V2_NIFI_MESSAGE
 
     return False
