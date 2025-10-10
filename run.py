bdb2325e3c38b9a27923994b8f0feb93c9204224
Jon Kent <29626876+Jon-Kent@users.noreply.github.com>
2025-10-10 15:50:55 +0100

    run

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
-from app import sdx_app, setup_keys, CONFIG
-from app.survey import process, get_tx_id_from_object_id
+from pathlib import Path
+
+from sdx_base.run import run
+from sdx_base.server.server import RouterConfig
+from sdx_base.server.tx_id import txid_from_pubsub
+
+from app.routes import router
+from app.settings import Settings
 
 if __name__ == '__main__':
-    setup_keys()
-    sdx_app.add_pubsub_endpoint(
-        process,
-        tx_id_getter=get_tx_id_from_object_id,
-        quarantine_topic_id=CONFIG.QUARANTINE_TOPIC_ID)
-
-    sdx_app.run(port=5000)
+    proj_root = Path(__file__).parent  # sdx-survey dir
+    router_config = RouterConfig(router, tx_id_getter=txid_from_pubsub)
+    run(Settings, routers=[router_config], proj_root=proj_root)
