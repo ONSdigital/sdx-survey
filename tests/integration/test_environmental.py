deda6799f25ea328b72e8d7f6e3e345040bf9d1f
Jon Kent <29626876+Jon-Kent@users.noreply.github.com>
2025-10-17 16:37:04 +0100

    refactor

--- tests/integration/test_environmental.py
+++ tests/integration/test_environmental.py
@@ -1,29 +1,25 @@
 from app.definitions.context import Context
 from app.definitions.context_type import ContextType
 from app.definitions.survey_type import SurveyType
-from tests.integration.test_base import TestBase, get_json
+from app.services.comments import CommentData
+from tests.integration.test_base import TestBase
 
 
 class TestEnvironmental(TestBase):
     def test_lcree(self):
-        submission_json = get_json("007.0009.json")
-        tx_id = submission_json["tx_id"]
-        self.set_survey_submission(tx_id, submission_json)
+        self.set_survey_submission("007.0009.json")
 
         resp = self.client.post("/", json=self.envelope)
 
-        # expected files
         expected_json_filename = "007_837e9fe2eab84909.json"
         expected_image_filename = "S837e9fe2eab84909_1.JPG"
         expected_index_filename = "EDC_007_20210203_837e9fe2eab84909.csv"
         expected_receipt_filename = "REC0302_837e9fe2eab84909.DAT"
 
-        # actual files
         actual_files = self.get_zip_contents()
 
-        # expected context
         expected_context: Context = {
-            "tx_id": tx_id,
+            "tx_id": "837e9fe2-eab8-4909-bc20-7fa599bf2210",
             "survey_type": SurveyType.ENVIRONMENTAL,
             "context_type": ContextType.BUSINESS_SURVEY,
             "survey_id": "007",
@@ -31,14 +27,15 @@ class TestEnvironmental(TestBase):
             "ru_ref": "15339216474W",
         }
 
-        # actual context
-        actual_context: Context = self.get_context()
-
-        # expected_receipt
         expected_receipt = {"caseId": "4bc5a896-994f-4c5d-b592-9da04147378a", "partyId": "UNKNOWN"}
 
-        # actual receipt
-        actual_receipt = self.get_receipt()
+        expected_comments: CommentData = {
+            'ru_ref': '15339216474W',
+            'boxes_selected': '',
+            'comment': None,
+            'additional': []}
+
+        expected_kind = "007_201605"
 
         self.assertTrue(resp.is_success)
         self.assertTrue(expected_json_filename in actual_files)
@@ -46,7 +43,7 @@ class TestEnvironmental(TestBase):
         self.assertTrue(expected_index_filename in actual_files)
         self.assertTrue(expected_receipt_filename in actual_files)
         self.assertEqual(4, len(actual_files))
-        self.assertEqual(expected_context, actual_context)
-        self.assertEqual(expected_receipt, actual_receipt)
-        # check comments were stored
-        self.mock_datastore.commit_entity.assert_called()
+        self.assertEqual(expected_context, self.get_context())
+        self.assertEqual(expected_receipt, self.get_receipt())
+        self.assertEqual(expected_comments, self.get_comment_data())
+        self.assertEqual(expected_kind, self.get_comment_kind())
